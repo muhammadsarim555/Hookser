efba289467e9bb88ba0b65d55845839a5c51c3ac
muhammadsarim555 <muhammadsarim555@gmail.com>
2020-03-15 22:31:23 +0500

    add Header & Footer reusable component

---
+++ src/components/HeaderFooter/index.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {View, Text, TouchableOpacity, Dimensions} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import HomeIcon from 'react-native-vector-icons/AntDesign';
+
+import styles from './style';
+
+function HeaderFooter(props) {
+  const {navigation, children} = props;
+
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerHeading}>Home Screen</Text>
+      </View>
+      <View style={styles.bodyContainer}>{children}</View>
+
+      <View style={styles.footerContainer}>
+        <View style={styles.footerSubContainer}>
+          <TouchableOpacity onPress={() => navigation.navigate('AdPost')}>
+            <HomeIcon name="home" size={30} color="#900" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('AdPost')}>
+            <Icon name="add-circle-outline" size={30} color="#900" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('AdPost')}>
+            <Icon name="person-outline" size={30} color="#900" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default HeaderFooter;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,25 +1,15 @@
 import * as React from 'react';
 import {View, Text, TouchableOpacity, Dimensions} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import AddIcon from 'react-native-vector-icons/Ionicons';
+
+import HeaderFooter from '../../components/HeaderFooter';
+import styles from './style';
 
 const {width, height} = Dimensions.get('window');
 
 function Home({navigation}) {
   return (
-    <View style={{flex: 1}}>
-      <View
-        style={{
-          flex: 0.9,
-          backgroundColor: 'white',
-          justifyContent: 'flex-end',
-          alignItems: 'center',
-        }}>
-        <Text style={{marginBottom: height * 0.01, fontSize: width * 0.04}}>
-          Home Screen
-        </Text>
-      </View>
-      <View style={{flex: 8.2}}>
+    <HeaderFooter navigation={navigation}>
+      <View>
         <TouchableOpacity onPress={() => navigation.navigate('AdPost')}>
           <Text>sss</Text>
         </TouchableOpacity>
@@ -30,34 +20,7 @@ function Home({navigation}) {
           <Text>sss</Text>
         </TouchableOpacity>
       </View>
-
-      <View
-        style={{
-          flex: 0.9,
-          backgroundColor: 'white',
-          borderTopColor: '#E3E5E9',
-          borderTopWidth: 2,
-          justifyContent: 'center',
-        }}>
-        <View
-          style={{
-            width: '90%',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignSelf: 'center',
-          }}>
-          <TouchableOpacity onPress={() => navigation.navigate('AdPost')}>
-            <Icon name="home" size={30} color="#900" />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('AdPost')}>
-            <AddIcon name="md-add-circle" size={30} color="#900" />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('AdPost')}>
-            <Icon name="user" size={30} color="#900" />
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
+    </HeaderFooter>
   );
 }
 
